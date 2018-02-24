b92d6d7085ae7e87699b6363c7d2363ab677a68d
Jon Nordby <jononor@gmail.com>
2018-02-24 20:13:37 +0100

    SDS011: Implement software device

--- components/ReadSDS011.hpp
+++ components/ReadSDS011.hpp
@@ -1,8 +1,13 @@
 #ifdef HAVE_SDS011
-//#include <somelib.h>
+//#include <SDS011-select-serial.h> // XXX: not safe to include twice, and config.h
+#include <SoftwareSerial.h>
 #else
 #warning "SDS011 support not enabled, you must define HAVE_SDS011"
 #endif
+
+// FIXME: make configurable
+const int RX_PIN = 11;
+const int TX_PIN = 10;
 
 /* microflo_component yaml
 name: ReadSDS011
@@ -23,26 +28,33 @@ microflo_component */
 class ReadSDS011 : public Component {
 private:
     Connection outPorts[ReadSDS011Ports::OutPorts::pm10+1];
+    SoftwareSerial serial;
+    SDS011 sds;
 
 public:
     ReadSDS011()
         : Component(outPorts, ReadSDS011Ports::OutPorts::pm10+1)
+        , serial(RX_PIN, TX_PIN)
+        , sds(serial)
     {
-
     }
 
     virtual void process(Packet in, MicroFlo::PortId port) {
         using namespace ReadSDS011Ports;
 
         if (port == InPorts::in) {
+            float pm25 = -999;
+            float pm10 = -666;
+            bool error = false;
 #ifdef HAVE_SDS011
-
+	        serial.begin(9600); // XXX: only do once?
+	        error = sds.read(&pm25, &pm10);
 #else
-            send((long)666, OutPorts::pm25);
-            send((long)999, OutPorts::pm10);
+            if (!error) {
+                send((long)(pm25*1000.0), OutPorts::pm25);
+                send((long)(pm10*1000.0), OutPorts::pm10);    
+            }
 #endif
         }
     }
 };
-
-
--- config.h
+++ config.h
@@ -2,9 +2,14 @@
 #define HAVE_DHT
 #include <DHT.h>
 
-// #define MICROFLO_MQTT_PREFIX ""
+#define HAVE_SDS011
+#include <SDS011-select-serial.h>
+#include <SoftwareSerial.h>
+
+
+#define MICROFLO_MQTT_HOST  "mqtt.bitraf.no"
+#define MICROFLO_MQTT_PREFIX "public/"
 // #define MICROFLO_MQTT_PORT 1883
-#define MICROFLO_MQTT_HOST  "test.mosquitto.org"
 
 #define MICROFLO_MSGFLO_ROLE "staub/mainhall"
 #define MICROFLO_MSGFLO_COMPONENT "FeinstaubSensor"
