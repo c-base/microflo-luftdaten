836e7444f22d50890871d4ab651bc5da6ea32b30
Jon Nordby <jononor@gmail.com>
2018-02-24 16:12:02 +0100

    ReadSDS: Provide fake dataa

--- components/ReadSDS11.hpp
+++ components/ReadSDS11.hpp
@@ -1,3 +1,9 @@
+#ifdef HAVE_SDS11
+//#include <somelib.h>
+#else
+#warning "SDS11 support not enabled, you must define HAVE_SDS11"
+#endif
+
 /* microflo_component yaml
 name: ReadSDS11
 description: Read particle concentration from a SDS011 sensor
@@ -14,11 +20,27 @@ outports:
     type: integer
     description: ""
 microflo_component */
-class ReadSDS11 : public SingleOutputComponent {
+class ReadSDS11 : public Component {
+private:
+    Connection outPorts[ReadSDS11Ports::OutPorts::pm10+1];
+
 public:
+    ReadSDS11()
+        : Component(outPorts, ReadSDS11Ports::OutPorts::pm10+1)
+    {
+
+    }
+
     virtual void process(Packet in, MicroFlo::PortId port) {
-        if (in.isData()) {
-            send(in, port);
+        using namespace ReadSDS11Ports;
+
+        if (port == InPorts::in) {
+#ifdef HAVE_SDS11
+
+#else
+            send((long)666, OutPorts::pm25);
+            send((long)999, OutPorts::pm10);
+#endif
         }
     }
 };
