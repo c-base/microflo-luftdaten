e42f9f0349cf1f1854e3e68c8d862a6de7657db4
Uwe Kamper <me@uwekamper.de>
2018-02-24 16:23:16 +0100

    DHT component started

--- components/ReadDHT22.hpp
+++ components/ReadDHT22.hpp
@@ -7,17 +7,45 @@ inports:
     description: "Trigger a reading"
 outports:
   temperature:
-    type: integer
+    type: float
     description: ""
   humidity:
-    type: integer
+    type: float
     description: ""
 microflo_component */
-class ReadDHT22 : public SingleOutputComponent {
+
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+class ReadDHT22 : public Component {
+private:
+    Connection outPorts[ReadDHT22Ports::OutPorts::humidity+1];
+    DHT dht;
 public:
+    ReadDHT22()
+        : Component(outPorts, ReadDHT22Ports::OutPorts::humidity+1)
+        , dht(DHTPIN, DHTTYPE)
+    {
+        //// Initialize DHT sensor for normal 16mhz Arduino
+        //dht = DHT(DHTPIN, DHTTYPE);
+    }
+    
     virtual void process(Packet in, MicroFlo::PortId port) {
-        if (in.isData()) {
-            send(in, port);
+        using namespace ReadDHT22Ports;
+        
+        float hum;  //Stores humidity value
+        float temp; //Stores temperature value
+        
+        if (port == InPorts::in) {
+            hum = dht.readHumidity();
+            temp = dht.readTemperature();
+            
+            send(temp, OutPorts::temperature);
+            send(hum, OutPorts::humidity);
+            
         }
     }
 };
