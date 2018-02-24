5fd322ae66c62a0335fec8048eb085584e375b6d
Uwe Kamper <me@uwekamper.de>
2018-02-24 16:58:28 +0100

    add HAVE_DHT preproccesor vars

--- components/ReadDHT22.hpp
+++ components/ReadDHT22.hpp
@@ -15,7 +15,9 @@ outports:
     description: ""
 microflo_component */
 
+#ifdef HAVE_DHT
 #include <DHT.h>;
+#endif
 
 //Constants
 #define DHTPIN 7     // what pin we're connected to
@@ -24,11 +26,15 @@ microflo_component */
 class ReadDHT22 : public Component {
 private:
     Connection outPorts[ReadDHT22Ports::OutPorts::humidity+1];
+#ifdef HAVE_DHT
     DHT dht;
+#endif
 public:
     ReadDHT22()
         : Component(outPorts, ReadDHT22Ports::OutPorts::humidity+1)
+#ifdef HAVE_DHT
         , dht(DHTPIN, DHTTYPE)
+#endif
     {
         //// Initialize DHT sensor for normal 16mhz Arduino
         //dht = DHT(DHTPIN, DHTTYPE);
@@ -37,13 +43,13 @@ public:
     virtual void process(Packet in, MicroFlo::PortId port) {
         using namespace ReadDHT22Ports;
         
-        float hum;  //Stores humidity value
-        float temp; //Stores temperature value
-        
         if (port == InPorts::in) {
+            float hum = -23.0;  //Stores humidity value
+            float temp = -300.0; //Stores temperature value
+#ifdef HAVE_DHT
             hum = dht.readHumidity();
             temp = dht.readTemperature();
-            
+#endif
             send(temp, OutPorts::temperature);
             send(hum, OutPorts::humidity);
             
