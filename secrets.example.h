3bf0f10c9f53f3195fe6c99952634d306d498eef
Jon Nordby <jononor@gmail.com>
2018-02-24 16:21:11 +0100

    Setup microfo config

---
+++ secrets.example.h
@@ -0,0 +1,11 @@
+
+// #define MICROFLO_MQTT_USER 0
+// #define MICROFLO_MQTT_PASSWORD 0
+
+#ifndef MICROFLO_WIFI_SSID
+#error "Missing wifi ssid. Define MICROFLO_WIFI_SSID"
+#endif
+
+#ifndef MICROFLO_WIFI_PASSWORD
+#error "Missing wifi password. Define MICROFLO_WIFI_PASSWORD"
+#endif
