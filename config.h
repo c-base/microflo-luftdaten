3696f3fcb247220a8bfe8b1b0d5c3df451f863ec
Henri Bergius <henri.bergius@iki.fi>
2018-02-26 11:44:19 +0100

    Define component namespace, refs msgflo/msgflo#168

--- config.h
+++ config.h
@@ -12,5 +12,5 @@
 // #define MICROFLO_MQTT_PORT 1883
 
 #define MICROFLO_MSGFLO_ROLE "staub/mainhall"
-#define MICROFLO_MSGFLO_COMPONENT "FeinstaubSensor"
+#define MICROFLO_MSGFLO_COMPONENT "c-base/FeinstaubSensor"
 
